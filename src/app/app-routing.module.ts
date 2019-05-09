1ee4f37319b605a1b1e4d0734d68e0ac4601b585
Alfredo <alfredotorre@gmail.com>
2019-05-09 19:22:27 +0100

    routing test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrimoComponent } from './primo/primo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'primo', component: PrimoComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimoComponent } from './primo/primo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimoComponent
   ],
   imports: [
     BrowserModule,

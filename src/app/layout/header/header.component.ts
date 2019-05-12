c2265f17c15879753d40f05aceefcb93b277206f
Alfredo <alfredotorre@gmail.com>
2019-05-12 19:49:59 +0100

    layout and svg

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  title = 'ingegnati';
+  title = 'ingegnati.it';
   motto = 'Sviluppatori che si ingegnano';
 
   constructor(public router: Router) { }
